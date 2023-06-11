53d13eb9a4833565faec4a79a751a308d6e9f7d3
Bakr Ibrahimi <bakribra5@gmail.com>
2023-06-11 12:11:19 +0100

    add task 1

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list) - 1:
+        return 'None'
+    else:
+        return my_list[idx]
