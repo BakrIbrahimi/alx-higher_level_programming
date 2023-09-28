d0e64997c6c1645a658155f4052854737e96e539
Bakr Ibrahimi <bakribra5@gmail.com>
2023-09-28 14:56:05 +0100

    add task0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sX GET "$1" -L
+# Takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI "$1" | grep "Content-Length" | cut -d' ' -f2
