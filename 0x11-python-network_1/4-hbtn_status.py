60c9cc831d752e3eb2a0f7acc8cf387253a09dac
Bakr Ibrahimi <bakribra5@gmail.com>
2023-09-29 17:19:37 +0100

    add task4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
+"""
+requests model
+"""
 
-"""Python script that fetches https://intranet.hbtn.io/status"""
-
-import requests
-
-if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    req = requests.get(url)
+if __name__ == '__main__':
+    import requests
+    html = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(req.text)))
-    print("\t- content: {}".format(req.text))
+    print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
